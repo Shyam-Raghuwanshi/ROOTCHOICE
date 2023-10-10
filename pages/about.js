2e77af9379acc668a3027b11e58d56823a80587c
shyamraghu <sr9993663832@gmail.com>
2023-10-10 15:53:21 +0530

    updating

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,107 @@
-import React, { useEffect } from 'react'
-
-const about = ({findMode}) => {
-  useEffect(() => {
-    findMode()
-  }, [])
-  
+import React from "react";
+import Link from "next/link";
+const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <>
+      <section className="w-full py-20">
+        <div className="max-w-2xl mx-auto">
+          <div className="bg-gray-900 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div>
+             
+              <div className="flex flex-col items-center pt-8">
+                <img
+                  className="mb-3 w-56 h-56 rounded-full shadow-lg"
+                  src="/static/images/logos/shyam.jpg"
+                  alt="shyam image"
+                />
+                <h3 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  Shyam Raghuwanshi
+                </h3>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Full-Stack Web-developer
+                </span>
+                <div className="flex mt-4 space-x-3 lg:mt-6">
+                  <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                 
+                    <a href="https://twitter.com/ShyamRa36109329" target="_blank" className="ml-3 cursor-pointer text-gray-200">
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                      </svg>
+                    </a>
+                    <a  href="http://instagram.com/shyam_raghuonec" target="_blank"  className="ml-3 cursor-pointer text-gray-200">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <rect
+                          width="20"
+                          height="20"
+                          x="2"
+                          y="2"
+                          rx="5"
+                          ry="5"
+                        ></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                      </svg>
+                    </a>
+                    <a href="https://www.linkedin.com/in/shyam-raghuwanshi-b67489220/" target="_blank" className="ml-3 cursor-pointer text-gray-200">
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="0"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="none"
+                          d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                        ></path>
+                        <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                      </svg>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className="font-mono p-4">
+                "Hey, I'm who you are looking for!"Experienced web developer
+                with a strong background in full-stack development. Proficient
+                in HTML, CSS, JavaScript, and various front-end frameworks.
+                Skilled in designing responsive and user-friendly interfaces.
+                Adept at backend technologies like Node.js and Express. Seeking
+                to leverage expertise in web development to create innovative
+                and seamless digital experiences for your company.
+                <Link
+                  href="https://shyamraghu.github.io/shyamportfolio.github.io/"
+                  target="_blank"
+                >
+                  <span className="text-blue-600 hover:underline">
+                    My portfolio website
+                  </span>
+                </Link>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default about
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import ImageScrollBar from '../components/ImageScrollbar'
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import ImageScrollBar from "../components/ImageScrollbar";
 
 export default function Home({ setSideCart }) {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    setSideCart(false)
-
-  }, [router.query])
+    setSideCart(false);
+  }, [router.query]);
 
   const images = [
     "./static/images/backgrounds/1.jpg",
@@ -19,18 +19,29 @@ export default function Home({ setSideCart }) {
     "./static/images/backgrounds/5.jpg",
     "./static/images/backgrounds/6.jpg",
     "./static/images/backgrounds/7.jpg",
-
-  ]
+  ];
 
   const showHTML = () => {
     return (
       <div className="p-8">
-        <h1 className="text-white text-[8rem] font-semibold mix-blend-exclusion">
+        <h1 className="text-white text-[8rem] font-semibold mix-blend-exclusion underline">
           ROOTCHOICE...
         </h1>
-        <h4 className="text-white text-center font-extrabold text-4xl mix-blend-exclusion">ROOTCHOICE! Your choice here:)</h4>
-      </div>)
-  }
+        <h4 className="text-white text-center font-extrabold text-4xl mix-blend-exclusion underline underline-offset-1">
+          ROOTCHOICE! Your choice here:)
+        </h4>
+        <div className="flex justify-center items-center pt-20">
+          <Link href={"/tshirts"}>
+            <div class="relative cursor-pointer px-6 py-3 font-bold text-white rounded-lg group">
+              <span class="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-gray-900 rounded-xl ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+              <span class="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-cyan-900 ease rounded-xl opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
+              <span class="relative font-serif">View Offers...</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <Head>
@@ -45,82 +56,165 @@ export default function Home({ setSideCart }) {
       <section className="text-gray-300 overflow-hidden body-font bg-gray-900 -mt-1 border-slate-100 ">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-300">ROOTCHOICE your choice hear :)</h1>
-            <p className="lg:w-1/2 w-full leading-relaxed text-gray-400">Whatever you want is here. All things in our ROOTCHOICE is cheaper then Flipkart, Amazon, Mantra etc.</p>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-300">
+              ROOTCHOICE your choice hear :)
+            </h1>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-400">
+              Whatever you want is here. All things in our ROOTCHOICE is cheaper
+              then Flipkart, Amazon, Mantra etc.
+            </p>
           </div>
           <div className="flex flex-wrap -m-4">
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">Shooting Stars</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  Shooting Stars
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <circle cx="6" cy="6" r="3"></circle>
                     <circle cx="6" cy="18" r="3"></circle>
                     <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">The Catalyzer</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">Neptune</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  Neptune
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">Melanchole</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  Melanchole
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">Bunker</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  Bunker
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
                     <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                   </svg>
                 </div>
-                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">Ramona Falls</h2>
-                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                <h2 className="text-lg text-gray-300 font-medium title-font mb-2">
+                  Ramona Falls
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waist co, subway
+                  tile poke farm.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
